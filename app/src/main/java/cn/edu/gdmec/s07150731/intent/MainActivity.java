31792cb7e97092560fea6bba10f91d71050ecf89
gdmec07150731 <907401314@qq.com>
2016-12-30 12:32:51 +0800

    补签

--- app/src/main/java/cn/edu/gdmec/s07150731/intent/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/s07150731/intent/MainActivity.java
@@ -1,49 +1,108 @@
 package cn.edu.gdmec.s07150731.intent;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.net.HttpURLConnection;
+        import java.net.MalformedURLException;
+        import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
+        import javax.net.ssl.HttpsURLConnection;
 
-    private EditText eText;
-    private Button button;
-    private TextView tText;
+        import android.app.Activity;
+        import android.content.Context;
+        import android.net.ConnectivityManager;
+        import android.net.NetworkInfo;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-    public MainActivity() {
-    }
+public class MainActivity extends Activity {
 
+    Button btn_test,btn_connect;
+    TextView tv;
+    EditText ed;
+    HttpsURLConnection conn;
+    URL url;
+    InputStream is;
+    String name;
+    ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo info = cm.getActiveNetworkInfo();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.intent_demo);
+        setContentView(R.layout.activity_main);
 
-        eText = (EditText) findViewById(R.id.editText);
-        button = (Button) findViewById(R.id.button);
-        tText = (TextView) findViewById(R.id.textView1);
+        btn_test = (Button) findViewById(R.id.button1);
+        tv = (TextView) findViewById(R.id.textView1);
+        ed = (EditText) findViewById(R.id.editText1);
+        btn_connect = (Button) findViewById(R.id.button2);
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+        btn_connect.setOnClickListener(new OnClickListener() {
+
             @Override
-            public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                bundle.putString("value",eText.getText().toString());
-                Intent intent = new Intent(MainActivity.this,Intent2.class);
-                intent.putExtras(bundle);
-                startActivityForResult(intent,10);
+            public void onClick(View arg0) {
+                // 连接
+                name=ed.getText().toString();
+                //服务器地址
+                String urlDate = "http://192.168.14.229:80/home/getInfo";
 
+                try {
+                    url = new URL(urlDate);
+                    try {
+                        conn = (HttpsURLConnection) url.openConnection();
+                        conn.connect();
+
+                        is= conn.getInputStream();
+                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                        String line = null;
+                        //创建sb对象储存所有数据。
+                        StringBuffer sb = new StringBuffer();
+                        while((line=br.readLine())!=null){
+                            sb.append(line);
+                        }
+
+                        //显示
+
+                        tv.setText(sb.toString());
+                        System.out.println(sb.toString());
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+
+                } catch (MalformedURLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
         });
-    }
 
-    protected void onActivityResult(int requestCode,int resultCode,Intent data){
-        switch (requestCode){
-            case 10:
-                Bundle bundle = data.getExtras();
-                tText.setText(bundle.getString("result"));
-        }
+
+        btn_test.setOnClickListener(new OnClickListener() {
+
+
+
+            @Override
+            public void onClick(View arg0) {
+                // 网络
+
+                if(info!=null){
+                    Toast.makeText(MainActivity.this, "联网正常"+info.getTypeName(), Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "未联网", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
